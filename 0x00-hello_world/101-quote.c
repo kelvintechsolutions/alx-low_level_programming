eb3faa0b0a664bcc69ec414c5e4e44e83f072b2b
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-06 17:34:52 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
+
 #include <unistd.h>
 
 /**
  * main -Entry point
-* Return: Always 1 (Success)
+* Return: Always 1 (Error)
  */
 int main(void)
 {
-         char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        write(2, str1, 59);
-        return (1);
+         char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
