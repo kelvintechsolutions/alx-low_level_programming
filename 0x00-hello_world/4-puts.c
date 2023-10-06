b028d46b3bec611d4694b635c88286670b915807
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-06 16:48:56 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,13 @@
 #include<stdio.h>
 /**
-   main-this is the starting point
-
-
-
-
-
-
-
-   Return will be 0 if it is successful
+ * *main - Entry point
+ *
+ * * Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
