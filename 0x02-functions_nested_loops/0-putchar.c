435b1b7ed94ac9ed72f27e59d56955a23b9fd021
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-11 15:42:16 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,25 @@
+#include "main.h"
+
 /**
  * main - Entry point
-* Return: 0
- */
-~    /**
- * main - Entry point
-* Return: 0
+ *
+ * Return: Always 0 
  */
 
 int main(void)
-{
-	char putchar[] = "_putchar";
-	int i = 0;
 
-	while (putchar[i] != '\0')
-	{
-		_putchar(putchar[i]);
-		i++;
-	}
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
+
 }
