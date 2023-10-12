b5da7b4021cddd30b67f48a73246d3f83bcb564b
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-12 07:20:55 -0700

    kelvin

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper - it checks for uppercase character.
+* @c: Variable text.
+* Return: Always 0.
+*/
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+
+	return (0);
+}
