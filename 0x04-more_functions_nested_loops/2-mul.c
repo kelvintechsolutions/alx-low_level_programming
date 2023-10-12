22915e7c96bff193eec55a38063a4472fed9f693
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-12 07:27:22 -0700

    nam

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul – print input.
+ * Description: it multiplies 2 numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: answer.
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
