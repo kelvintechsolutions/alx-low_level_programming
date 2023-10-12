cfa6cfa05d150895dd0820368ec2186bc1d08b6a
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-12 07:02:11 -0700

    namu kelvin

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - it  checks uppercase.
+ * @c: parameter.
+ * Return: 0 if sucess happens.
+ *
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
