b9c6493eb13dd943348cd0674ec21eb190efed59
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-12 08:15:39 -0700

    namu

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers – it prints more numbers.
+ * Return: 0 if success.
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+
+			_putchar((y % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
