c2042283832333f37d9ba31724de16443c03efcd
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-20 13:45:05 -0700

    tew

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,34 @@
 #include "main.h"
+#include "main.h"
 
 /**
-* _strncpy – copies a string.
-* @dest: it is the destination string.
-* @src: it is the source string.
-* @n: the number of bytes to copy.
-*
-* Return: poiter to the resulting string.
-*/
+  * _strncpy - Copy a string.
+  * @dest:  destination value.
+  * @src:  source value.
+  * @n:  copy limit.
+  *
+  * Return: char value.
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
