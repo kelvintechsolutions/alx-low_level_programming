531d30d00e1e78655567bb50da46b492caead1d3
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-23 07:33:25 -0700

    mmemme

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,21 @@
 #include "main.h"
+
 /**
- * _memset -it fills a block of memoy with a specific value.
- * @s: it is the starting addresses of memoy to be filled.
- * @b: it is the desiredd value.
- * @n: it is the number of bytes to be changedd.
- *
- * Return: it is the changed array with new value for n byte.
- */
+ * *_memset -it  fills memoy with a byte.
+ * @s: memoy area to be filled.
+ * @b: it is the char to copy.
+ * @n: the nuber of times to copy b.
+ *
+ * Return: poiter to the memoy area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
+	unsigned int i;
 
-for (; n > 0; i++)
-{
-s[i] = b;
-n--;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
-return (s);
-}
-
-
