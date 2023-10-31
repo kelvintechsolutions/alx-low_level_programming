3854d7dba0b3e914a917495c5f35877cbbefdc96
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-10-31 04:44:52 -0700

    free grid questtion

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid – it is a fuction that frees a 2-dimensional grideed
+ * previously created by your (alloc_grid function).
+ * @grid:it is a  double poiter to the grind.
+ * @height:it is the  height of the grind.
+ * Return:it is an  address of the newly allocated memoy.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int h;
+
+	for (h = 0; h < height; h++)
+	{
+		free(grid[h]);
+	}
+	free(grid);
+}
