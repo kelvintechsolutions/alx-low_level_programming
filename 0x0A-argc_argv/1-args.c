c6b98cbf6b4d71b87daf59eecd829778d70cf874
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-11-01 05:26:28 -0700

    frist position

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,18 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * main - print the nuber of args .
+ * @argc:it is the argumet count.
+ * @argv:it is the argumet vector.
+ * Return: 0 if successful.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
