57539584a28f71f124e185614692ee49b82527c0
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-11-06 04:03:52 -0800

    this is task 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -it is  a program thats prints/calls out its name.
+ * Return: 0 if successful.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
