648e4b20bbb36f50c89b1b812a1afa9fb73e8320
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-11-07 22:51:22 -0800

    optional

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -it will print the opcode of it own fuction.
+ * @argc: argument counter.
+ * @argv: argument vector.
+ * Return:exit 1 ( one arg is not correct), 2 ( byte is negative.)
+ */
+int main(int argc, char *argv[])
+{
+	short bytes, x;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	printf("%02x", *((unsigned char *) (main)));
+	for (x = 1; x < bytes; ++x)
+		printf(" %02x", *((unsigned char *) (main + x)));
+	putchar('\n');
+	exit(EXIT_SUCCESS);
+}
