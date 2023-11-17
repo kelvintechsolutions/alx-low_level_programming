f1be18f1b1c7e1daa0018754dc50a90f7c2e8e54
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-11-17 13:40:08 -0800

    updating everything

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * before_main -the function will be executed before main.
+ * Return:no return expected.
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
