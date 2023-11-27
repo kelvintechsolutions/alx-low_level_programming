57650c4cae150f1818ee294c3acdbafe55c1cc90
kelvintechsolutions <kevohmutugi51@gmail.com>
2023-11-27 03:46:46 -0800

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,32 @@
 #include "main.h"
 
 /**
-  * create_file-creates a file and fills it with text.
-  * @filename:it is a type char filename poiter.
-   *@text_content:type char pointer of char.
-  */
+* create_file -it will create a file and fills it with text.
+* @filename: name of the file to create.
+* @text_content:it is the text to write in the file.
+* Return: it will return 1 if  success and-1 if failure.
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, t, s = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
 
 	if (text_content)
-		write(fd, text_content, _strlen(text_content));
+	{
+		while (text_content[s])
+			s++;
+		t = write(fd, text_content, s);
+		if (t != s)
+			return (-1);
+	}
 
 	close(fd);
+
 	return (1);
-}
-
-/**
-  * _strlen -itreturns the length of a strig.
-  * @s:it is the string to count.
-  * Return:it will return the string length.
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
