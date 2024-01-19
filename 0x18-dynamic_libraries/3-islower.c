c64dc5b3b38cc3c97641a76b14183a54533637c3
https://github.com/kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-19 10:48:34 -0800

    adavanced task

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower -it prints 1 or 0 depending on input.
+ * @c:it is the first parameter.
+ * Description:it prints all lowercase letters.
+ * Return:it returns always(0).
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
