9b41d50a3c971f16e91692ad5b8175699dad1f5a
https://github.com/kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-19 11:15:26 -0800

    task 1

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit -it checks for a digit (0 through 9)
+ * @c:it is the int to be checked.
+ * Return:it returns 1 if c is a digit,
+ * 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
