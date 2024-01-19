4d5282739814115f64d02cfc0c18d12f113a9ff0
https://github.com/kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-19 11:31:41 -0800

    more task

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -it checks the code.
+ * 
+ * Return:it  always returns EXIT_SUCCESS;
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn lib"));
+    return (EXIT_SUCCESS);
+}
