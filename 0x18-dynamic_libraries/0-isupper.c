329482d480fca12d0d04a53c0af6b845a5f26e72
https://github.com/kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-19 11:33:24 -0800

    more task

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper -it checks for uppercase character.
+ * @c: it is the character to be checked
+ * Return:it returns 1 if c is uppercase,
+ * 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
